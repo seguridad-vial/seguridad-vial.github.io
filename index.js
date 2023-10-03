f451e8257d1564276654349f01d4a8f466b43500
seguridad-vial <145610562+seguridad-vial@users.noreply.github.com>
2023-10-03 16:57:40 -0500

    sonidos

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ lib.properties = {
 	color: "#B2FFFF",
 	opacity: 1.00,
 	manifest: [
-		{src:"images/index_atlas_1.png?1695912252603", id:"index_atlas_1"}
+		{src:"images/index_atlas_1.png?1695915284173", id:"index_atlas_1"}
 	],
 	preloads: []
 };
